cdc9eda52d8f98089d9d2ec484d5f50e59b1b940
Imamgg <404imamgg@gmail.com>
2025-12-12 09:33:29 +0700

    change dir

--- src/grade/grade.service.ts
+++ src/grade/grade.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Grade } from "./entities/grade.entity";
 import { CreateGradeDto, UpdateGradeDto } from "./dto/grade.dto";
-import { RabbitmqService } from "./rabbitmq/rabbitmq.service";
+import { RabbitmqService } from "../rabbitmq/rabbitmq.service";
 
 @Injectable()
 export class GradeService {
