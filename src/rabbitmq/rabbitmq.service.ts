8b7c4decc3a7fcbc2882eb87e6869d7b76de1578
Imamgg <404imamgg@gmail.com>
2025-12-12 09:55:00 +0700

    change dir

--- src/rabbitmq/rabbitmq.service.ts
+++ src/rabbitmq/rabbitmq.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import * as amqp from 'amqplib';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import * as amqp from "amqplib";
 
 @Injectable()
 export class RabbitmqService {
@@ -13,13 +13,13 @@ export class RabbitmqService {
 
   async connect() {
     try {
-      this.connection = await amqp.connect(
-        this.configService.get('RABBITMQ_URL'),
-      );
-      this.channel = await this.connection.createChannel();
-      console.log('Connected to RabbitMQ');
+      this.connection = (await amqp.connect(
+        this.configService.get("RABBITMQ_URL")
+      )) as amqp.Connection;
+      this.channel = (await this.connection.createChannel()) as amqp.Channel;
+      console.log("Connected to RabbitMQ");
     } catch (error) {
-      console.error('Failed to connect to RabbitMQ:', error);
+      console.error("Failed to connect to RabbitMQ:", error);
     }
   }
 
@@ -31,15 +31,15 @@ export class RabbitmqService {
       });
       console.log(`Message sent to queue ${queue}`);
     } catch (error) {
-      console.error('Error publishing to queue:', error);
+      console.error("Error publishing to queue:", error);
     }
   }
 
   async publishGradeNotification(data: any) {
-    await this.publishToQueue('grade_notifications', data);
+    await this.publishToQueue("grade_notifications", data);
   }
 
   async publishReportGeneration(data: any) {
-    await this.publishToQueue('report_generation', data);
+    await this.publishToQueue("report_generation", data);
   }
 }
